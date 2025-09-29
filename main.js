4ee21ceb9e5fc99c11fc9dd569316c31bd0c40dd
xsin-dev <sayfullayev0510@gmail.com>
2025-09-29 23:06:52 +0500

    eco-second

--- main.js
+++ main.js
@@ -159,3 +159,410 @@ heroDrawer.addEventListener("click", (e) => {
         document.body.style.overflow = "";
     }
 });
+
+
+// TESTIMONIALS CAROUSEL ======================================================================================================
+const testimonialsNext = document.querySelector('.testimonials__header-button--next')
+const testimonialsPrev = document.querySelector('.testimonials__header-button--prev')
+const testimonialsCarousel = document.querySelector('.testimonials__cards')
+
+const dots = document.querySelector('.testimonials__dots')
+
+
+let currentIndex = 0;
+let interval;
+
+const testimonials = [
+    {
+        id: 1,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. Donec sed neque eget",
+        authorName: "Robert Fox",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user1.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 5,
+    },
+    {
+        id: 2,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. ",
+        authorName: "Dianne Russell",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user2.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 3
+    },
+    {
+        id: 3,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. ",
+        authorName: "Eleanor Pena",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user3.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 4
+    },
+    {
+        id: 2,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. ",
+        authorName: " Russell",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user2.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 3
+    },
+    {
+        id: 3,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. ",
+        authorName: "Eleanor",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user3.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 4
+    },
+    {
+        id: 2,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. ",
+        authorName: " Russell",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user2.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 3
+    },
+    {
+        id: 3,
+        quote: "Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna dictum, bibendum cursus velit sodales. DPellentesque eu nibh eget mauris congue mattis mattis nec tellus. ",
+        authorName: "Eleanor",
+        authorRole: "Customer",
+        authorImage: "./assets/images/png/testimonials-user3.jpg",
+        quotation: './assets/images/svg/quotation-icon.svg',
+        rating: 4
+    },
+]
+
+
+testimonials.map((item, index) => {
+    // CREATE ELEMENTS
+    const testimonialCard = document.createElement('div')
+    const testimonialCardQuotation = document.createElement('img')
+    const testimonialCardText = document.createElement('p')
+    const testimonialCardBottom = document.createElement('div')
+    const testimonialCardUser = document.createElement('div')
+    const testimonialCardUserImg = document.createElement('img')
+    const testimonialCardUserInfo = document.createElement('div')
+    const testimonialCardUserName = document.createElement('p')
+    const testimonialCardUserRole = document.createElement('p')
+    const testimonialCardRating = document.createElement('div')
+
+    // ADD Classlist
+    testimonialCard.classList.add('testimonials__card')
+    testimonialCardQuotation.classList.add('testimonials__card-quotation')
+    testimonialCardText.classList.add('testimonials__card-text')
+    testimonialCardBottom.classList.add('testimonials__card-bottom')
+    testimonialCardUser.classList.add('testimonials__card-user')
+    testimonialCardUserImg.classList.add('testimonials__card-user-img')
+    testimonialCardUserInfo.classList.add('testimonials__card-user-info')
+    testimonialCardUserName.classList.add('testimonials__card-user-name')
+    testimonialCardUserRole.classList.add('testimonials__card-user-position')
+    testimonialCardRating.classList.add('testimonials__card-rating')
+
+    // OBYEKT bilan bog'lash
+    testimonialCardQuotation.src = item.quotation
+    testimonialCardText.textContent = item.quote
+    testimonialCardUserImg.src = item.authorImage
+    testimonialCardUserName.textContent = item.authorName
+    testimonialCardUserRole.textContent = item.authorRole
+
+    // Yulduzchalarni yaratish
+    for (let i = 0; i < 5; i++) {
+        const star = document.createElement('i')
+
+        if (i < item.rating) {
+            star.classList.add("fa-solid", "fa-star")
+        } else {
+            star.classList.add('fa-regular', 'fa-star')
+        }
+        testimonialCardRating.append(star)
+    }
+
+    // ======== DOTS ====
+    if (index <= testimonials.length - 3) {
+        const dot = document.createElement('div')
+        dot.classList.add('testimonials__dot')
+
+        dot.addEventListener('click', () => {
+            currentIndex = index;
+            testimonialsCarousel.style.transform = `translateX(-${currentIndex * 33.33}%)`
+
+            dots.querySelectorAll('.testimonials__dot').forEach((dot) => {
+                dot.classList.remove('testimonials__dot--active')
+            })
+            dots.querySelectorAll('.testimonials__dot')[currentIndex].classList.add('testimonials__dot--active')
+
+            startCarousel()
+        })
+        if (index === currentIndex) {
+            dot.classList.add('testimonials__dot--active')
+        }
+        dots.appendChild(dot)
+    }
+    // =================
+
+    // APPEND qilish yani qo'shish HTML ga 
+    testimonialCardUserInfo.append(testimonialCardUserName, testimonialCardUserRole)
+
+    testimonialCardUser.append(testimonialCardUserImg, testimonialCardUserInfo)
+
+    testimonialCardBottom.append(testimonialCardUser, testimonialCardRating)
+
+    testimonialCard.append(testimonialCardQuotation, testimonialCardText, testimonialCardBottom)
+
+    testimonialsCarousel.appendChild(testimonialCard, dots)
+})
+
+// Next button
+testimonialsNext.addEventListener('click', () => {
+    if (currentIndex === testimonials.length - 3) {
+        currentIndex = 0
+    } else {
+        currentIndex++;
+
+    }
+    updateCarousel()
+    startCarousel()
+})
+// Prev button
+testimonialsPrev.addEventListener('click', () => {
+    if (currentIndex === 0) {
+        currentIndex = testimonials.length - 3
+    } else {
+        currentIndex--;
+    }
+    updateCarousel()
+    startCarousel()
+})
+// ======= startCarousel chaqirildi ======
+startCarousel()
+// =======================================
+
+// bir nechta joyda qaytarilgan amalni bitta funksiyaga olish 
+function updateCarousel() {
+    testimonialsCarousel.style.transform = `translateX(-${currentIndex * 33.33}%)`
+    dots.querySelectorAll('.testimonials__dot').forEach((dot) => {
+        dot.classList.remove('testimonials__dot--active')
+    })
+    dots.querySelectorAll('.testimonials__dot')[currentIndex].classList.add('testimonials__dot--active')
+}
+
+// SetInterval ishlatish funksiyasi
+function startCarousel() {
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        if (currentIndex === testimonials.length - 3) {
+            currentIndex = 0
+        } else {
+            currentIndex++;
+
+        }
+        updateCarousel()
+    }, 2000)
+}
+// =======================================================================================================================
+
+// ====================================================== CARDS APPEND ===================================================
+const productsCards = document.querySelector('.products__cards')
+
+function createProductCard(item) {
+    const productsCard = document.createElement('div');
+    const productsImage = document.createElement('div');
+    const productsImg = document.createElement('img');
+    const productsContent = document.createElement('div');
+    const productsInfo = document.createElement('div');
+    const productsName = document.createElement('p');
+    const productsPriceBox = document.createElement('div');
+    const productsPrice = document.createElement('p');
+    const productsPriceOld = document.createElement('p');
+    const productsCart = document.createElement('div');
+    const productsCartIcon = document.createElement('i');
+    const productsCardAction = document.createElement('div');
+    const productsCardEye = document.createElement('div');
+    const productsCardEyeIcon = document.createElement('i');
+    const productsCardLike = document.createElement('div');
+    const productsCardLikeIcon = document.createElement('i');
+    const productsBadge = document.createElement('div');
+    const productsBadgePercentage = document.createElement('p');
+
+    // class qo'shish
+    productsCard.classList.add('products__card');
+    productsImage.classList.add('products__card-image');
+    productsImg.classList.add('products__card-img');
+    productsContent.classList.add('products__card-content');
+    productsInfo.classList.add('products__card-info');
+    productsName.classList.add('products__card-title');
+    productsPriceBox.classList.add('products__card-price-box');
+    productsPrice.classList.add('products__card-price');
+    productsPriceOld.classList.add('products__card-old-price');
+    productsCart.classList.add('products__card-cart');
+    productsCardAction.classList.add('products__card-action');
+    productsCardLike.classList.add('products__card-like');
+    productsCardEye.classList.add('products__card-eye');
+    productsBadge.classList.add('products__card-badge');
+    productsBadgePercentage.classList.add('products__card-badge-percentage');
+
+    // qiymatlar
+    productsName.textContent = item.name;
+    productsImg.src = item.image;
+    productsPrice.textContent = `$${item.price.toFixed(2)}`;
+
+    // agar discount bo‘lsa
+    if (item.discount) {
+        const oldPrice = item.price / (1 - item.discount / 100);
+        productsPriceOld.textContent = `$${oldPrice.toFixed(2)}`;
+        productsPriceBox.append(productsPrice, productsPriceOld);
+
+        // badge chiqarish
+        productsBadgePercentage.textContent = `Sale ${item.discount}%`;
+        productsBadge.appendChild(productsBadgePercentage);
+        productsCard.appendChild(productsBadge);
+    } else {
+        // discount bo‘lmasa faqat price ko‘rsatiladi
+        productsPriceBox.append(productsPrice);
+    }
+
+    // ikonlar
+    productsCardLike.innerHTML = '<i class="fa-regular fa-heart"></i>';
+    productsCardEye.innerHTML = '<i class="fa-regular fa-eye"></i>';
+    productsCart.innerHTML = '<i class="fa-solid fa-cart-shopping"></i>';
+
+    // strukturaga yig‘ish
+    productsImage.appendChild(productsImg);
+    productsInfo.append(productsName, productsPriceBox);
+    productsContent.append(productsInfo, productsCart);
+    productsCardAction.append(productsCardLike, productsCardEye);
+    productsCard.append(productsImage, productsContent, productsCardAction);
+
+    return productsCard;
+}
+
+const products = [
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        discount: 30,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+    {
+        id: 1,
+        name: "Green apple",
+        price: 14.99,
+        // discount: 50,
+        image: './assets/images/png/green-apple.jpg',
+        rating: 4,
+        isFavorite: false,
+    },
+]
+
+products.forEach((item) => {
+    const card = createProductCard(item)
+
+    productsCards.appendChild(card)
+})
+
+
+
+
+const dealsCards = document.querySelector('.deals__cards')
+products.forEach((item) => {
+    const card = createProductCard(item)
+    dealsCards.appendChild(card)
+})
+
+
+const featuredProductsCards = document.querySelector('.featured-products__cards');
+products.slice(0, 4).forEach((item) => {
+    const card = createProductCard(item);
+    featuredProductsCards.appendChild(card);
+});
